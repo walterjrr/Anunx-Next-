ca28736bb12bff45019a9ecefc69a586b1ea579e
Walter Junior <80055998+walterjrr@users.noreply.github.com>
2022-03-24 23:06:49 -0300

    Estruturando o Projeto

---
+++ src/templates/Default.js
@@ -0,0 +1,12 @@
+import Header from '../components/Header'
+
+const Default = ({children}) => {
+  return (
+      <>
+        <Header />
+        {children}
+        <footer>FOOTER</footer>
+      </>
+  )
+}
+export default Default
