0025ad5ffe3ca191c25cb7a06b34764698f54340
walterjrr <jrwalter731@gmail.com>
2022-04-06 14:56:37 -0300

    bugs fixs mongoDb

--- src/utils/dbConnect.js
+++ src/utils/dbConnect.js
@@ -30,6 +30,7 @@ async function dbConnect() {
     }
 
     cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+      console.log("Connected to Databased")
       return mongoose
     })
   }
