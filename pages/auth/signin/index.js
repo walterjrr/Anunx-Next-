cbf87af4965d8db938d9f9df5fbdba594982a3b8
walterjrr <jrwalter731@gmail.com>
2022-04-18 18:19:55 -0300

    login com credencial do google parte 1

--- pages/auth/signin/index.js
+++ pages/auth/signin/index.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { Formik } from "formik";
 import axios from "axios";
 import { useRouter } from "next/router";
@@ -29,6 +30,12 @@ const Signin = () => {
 
   console.log(session);
 
+  const handleGoogleLogin = () => {
+    signIn("google", {
+      callbackUrl: "http://localhost:3000/user/dashboard",
+    });
+  };
+
   const handleFormSubmit = (values) => {
     signIn("credentials", {
       email: values.email,
@@ -52,6 +59,28 @@ const Signin = () => {
 
       <Container>
         <Box className={classes.box}>
+          <Box display="flex" justifyContent="center">
+            <Button
+              variant="contained"
+              color="primary"
+              startIcon={
+                <Image
+                  src="/images/logo_google.png"
+                  width={20}
+                  height={20}
+                  alt="Login com Google"
+                />
+              }
+              onClick={handleGoogleLogin}
+            >
+              Entrar com Google
+            </Button>
+          </Box>
+
+          <Box className={classes.orSeparator}>
+            <span>ou</span>
+          </Box>
+
           <Formik
             initialValues={initialValues}
             validationSchema={validationSchema}
